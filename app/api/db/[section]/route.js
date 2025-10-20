4d124cc596295daaebe24be82085faab690c6a26
Mehdi <khosronickkhah@gmail.com>
2025-10-20 12:50:35 +0330

    fix: import static db.json

--- app/api/db/[section]/route.js
+++ app/api/db/[section]/route.js
@@ -1,17 +1,11 @@
-import fs from "fs";
-import path from "path";
+import data from "../../../../db.json";
 
 export async function GET(req, { params }) {
-  const { section } = params; // مثلاً header، tea، rice و غیره
-  const filePath = path.join(process.cwd(), "db.json");
-  const fileData = fs.readFileSync(filePath, "utf-8");
-  const data = JSON.parse(fileData);
+  const { section } = params;
 
-  // اگر اون بخش وجود نداشت، خطا برگردون
   if (!data[section]) {
     return Response.json({ error: "Section not found" }, { status: 404 });
   }
 
-  // فقط همون بخش خاص از فایل رو بده
   return Response.json(data[section]);
 }
